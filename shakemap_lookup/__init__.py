10dcfd12541e7fe1e29fa35dde978c393a099bcc
Iain Bailey <iainbailey@gmail.com>
2019-12-14 11:50:26 +0000

    addded a couple more functions from usgs_web to the main part

--- shakemap_lookup/__init__.py
+++ shakemap_lookup/__init__.py
@@ -1,4 +1,5 @@
 from .usgs_web import search_usgsevents
+from .usgs_web import query_shakemapdetail, get_shakemapgrid_url
 from .usgs_web import download_shakemapgrid
 from .usgs_shakemap_grid import USGSshakemapGrid
 from .locations import Locations
