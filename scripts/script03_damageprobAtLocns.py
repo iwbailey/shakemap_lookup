b8eb04f32018c28f4a998ee0dac8de2818667c41
Iain Bailey <iainbailey@gmail.com>
2020-02-27 22:07:20 +0000

    added arguments, cleaned up for consistency with other scripts

--- scripts/script03_damageprobAtLocns.py
+++ scripts/script03_damageprobAtLocns.py
@@ -10,7 +10,9 @@ probability of different damage states
 Saves results
 
 """
-# Libraries ------------------------------------------------------------------
+import argparse as ap
+import pandas as pd
+
 import os
 import yaml
 import numpy as np
@@ -21,123 +23,92 @@ from shakemap_utils import USGSshakemapGrid
 from shakemap_utils import Locations
 from shakemap_utils import FragilityCurve
 
-# Parameters ------------------------------------------------------------------
 
-ifile_params = "lookup_params.yaml"
+def get_args():
+    """Get script arguments"""
 
+    parser = ap.ArgumentParser(description='Lookup shakemap values at a set of locations, get damage estimate',
+                               formatter_class=ap.ArgumentDefaultsHelpFormatter)
 
-# Inline functions ------------------------------------------------------------
+    parser.add_argument('-i', '--ifile',
+                        metavar='input_coordinates.csv',
+                        type=str,
+                        nargs='?',
+                        help='.csv file with columns "lon" and "lat"')
 
+    parser.add_argument('-s', '--shakemap',
+                        metavar='shakemap_grid.xml',
+                        type=str,
+                        nargs='?',
+                        help='ShakeMap grid stored in xml or xml.zip format')
 
-def checkplot_interpolatedamage(frag, locns):
-    """Read in fragility function, compare to the interpolated results we've output
-    to file
+    parser.add_argument('-u', '--shakemap_unc',
+                        metavar='shakemap_unc_grid.xml',
+                        type=str,
+                        nargs='?',
+                        default=None,
+                        help='ShakeMap grid stored in xml or xml.zip format')
 
-    Plot the fragility curves as read in
-    IN: Pandas data frame
-          column 1 is named 'MMI'
-          columns 2+ are named the damage states
-    OUT: pyplot figure handle
-    """
+    parser.add_argument('--fragility_file',
+                        metavar='fragility.csv',
+                        type='str',
+                        nargs='?',
+                        help='Fragility file in csv format')
 
-    fig, ax = plt.subplots(1, 1, facecolor='white')
+    parser.add_argument('-o', '--ofile',
+                        metavar='lookup_results.csv',
+                        type=str,
+                        nargs='?',
+                        default='locations_with_intensity.csv',
+                        help='Output filename for locations with their shakemap intensity.')
 
-    # Plot underlying fragility curve
-    for c in frag.damagestates():
-        ax.plot(frag.intensities, frag.exceedprob[c].values, label=c)
+    args = parser.parse_args()
 
-    # Plot result of the location lookup
-    fnmIntens = frag.intensitymeasure + '_med'
-    for c in frag.damagestates():
-        # Fieldname for the probabilities at these locations
-        fnmDamage = 'prob_' + c
-        ax.plot(locns.df[fnmIntens].values, locns.df[fnmDamage].values, '+',
-                label='locations')
-
-    ax.set_xlabel(frag.intensitymeasure)
-    ax.set_ylabel('ProbExceedance')
-    ax.grid('on')
-    ax.legend(loc=2, frameon=False, framealpha=0.5)
-
-    plt.title('Check fragility calculation')
-
-    # Show the plot
-    print('Pausing while plot is shown...')
-    pylab.show(block=True)
-
-    return
+    return args
 
 
-# Script ----------------------------------------------------------------------
-
-
-def main():
-
-    """
-    MAIN SCRIPT:
-
-    """
-
-    # Read the parameters from the file
-    with open(ifile_params, 'r') as stream:
-        try:
-            # Expect the yaml file to contain fields that go into a dict
-            lookupParams = yaml.load(stream)
-        except yaml.YAMLError as exc:
-            print(exc)
-            sys.exit()
-
-    # Shakemap and uncertainty files
-    ifile_sm = os.path.join(lookupParams['idir'],
-                            ('grid_%s.xml.zip' %
-                             lookupParams['eventid']))
-    ifile_unc = os.path.join(lookupParams['idir'],
-                             ('uncertainty_%s.xml.zip' %
-                              lookupParams['eventid']))
-
-    # Which intensity measure to use
-    intensMeasure = lookupParams['intensMeasure']
-
-    # Read Shakemap into class object
-    print("Reading shakemap from file...")
-    shakemap = USGSshakemapGrid(ifile_sm, intensMeasure, ifile_unc)
-
-    # Read locations into pandas array
-    print("Reading locations from file...")
-    locns = Locations(lookupParams['ifile_locns'])
-    print("\t...%i locations" % len(locns.df))
-
-    # Look up the intensities at the locations
-    locns.add_intensities(shakemap)
-    print("\t...%i locations with an intensity" %
-          locns.df[intensMeasure + '_med'].count())
-
-    # Remove locations where no intensity is found
-    print("Removing %i locations without any intensity" %
-          sum(np.isnan(locns.df[intensMeasure + '_med'])))
-    locns.df = locns.df[~np.isnan(locns.df[intensMeasure + '_med'])]
+def main(args=get_args()):
+    """Main Script."""
 
     # Read fragility file into class object
     print("Reading fragility curves from file...")
-    frag = FragilityCurve(lookupParams['ifile_frag'])
+    frag = FragilityCurve(args.fragility_file)
+
+    # Read Shakemap into class object
+    print("Reading shakemap from file...")
+    if args.shakemap_unc is not None:
+        shakemap = USGSshakemapGrid(args.shakemap, frag.intensity_measure, args.shakemap_unc)
+    else:
+        shakemap = USGSshakemapGrid(args.shakemap, frag.intensity_measure)
+
+    # Read locations into pandas array
+    locns = pd.read_csv(args.ifile)
+    print("\t...%i locations" % len(locns))
+
+    # Get the mean/median and std deviation from the shakemap.
+    median, stddev = shakemap.lookup(locns['lon'].values, locns['lat'].values)
+    locns[frag.intensity_measure] = median
+    print(f"\t...{locns[frag.intensity_measure].count():d} locations with an intensity")
+
+    # Remove locations where no intensity is found
+    print("Removing %i locations without any intensity" %
+          sum(np.isnan(locns[frag.intensity_measure])))
+    locns = locns[~np.isnan(locns[frag.intensity_measure])]
 
     # Convert intensities to probability of damage at each state
     print("Getting damage at locations...")
-    locns.add_damageprobs(intensMeasure, frag, useMedian=True)
+    # Loop through all damage states in the fragility curve
+    for c in frag.damagestates():
+        # Get the result of the interpolation, allow values over the max by setting them equal to the max.
+        locns['prob_' + c] = frag.interp_damagestate(locns[frag.intensity_measure], c)
 
     print(frag.mindamagestate())
     print("\t...%i locations with a chance of damage" %
-          sum(locns.df['prob_' + frag.mindamagestate()] > 0.0))
-    if(lookupParams['isKeepOnlyDamaged']):
-        print("\t...Keeping only locations with chance of damage")
-        locns.df = locns.df[locns.df['prob_' + frag.mindamagestate()] > 0.0]
+          sum(locns['prob_' + frag.mindamagestate()] > 0.0))
 
     # Write the output file
-    locns.df.to_csv(lookupParams['ofile_locns'], index=False)
-    print("Written location details to %s" % lookupParams['ofile_locns'])
-
-    # Generate check plots
-    checkplot_interpolatedamage(frag, locns)
+    locns.to_csv(args.ofile, index=False)
+    print(f"Written location details to {args.ofile}")
 
     print("Finished")
 
